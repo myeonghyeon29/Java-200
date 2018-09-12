8f11591b3eaa49d13fee0a4ba9a449db5f3f9c9d
pc1 <pc1@pc>
2018-09-12 11:24:39 +0900

    섭씨를 화씨로 바꾸기

---
+++ src/java200/middle/Temperature.java
@@ -0,0 +1,14 @@
+package java200.middle;
+
+public class Temperature {
+
+	public static void main(String[] args) {
+		// 0µµ ºÎÅÍ 100µµ±îÁö º¯°æ
+		for (int i=0; i<101; i++) {
+			double fahrenheit = 9.0/5*i + 32;
+			System.out.printf("¼·¾¾ %dµµ = È­¾¾ %.2fµµ \n", i, fahrenheit);
+		}
+
+	}
+
+}
