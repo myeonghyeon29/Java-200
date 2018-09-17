9225bc639e84f1ea143a7adc8b5d9230ab915bbc
YanjuLee <43159295+YanjuLee@users.noreply.github.com>
2018-09-17 17:22:45 +0900

    Update Gradd.java
    Java에서 printf 사용을해보자

--- src/java200/elementary/Gradd.java
+++ src/java200/elementary/Gradd.java
@@ -8,9 +8,11 @@ public class Gradd {
 		int math = 89;
 		int tot = kor + eng + math;
 		int average = tot / 3;
-		System.out.printf("±¹¾î %d, ¿µ¾î %d, ¼öÇĞ %d\t", kor, eng, math);
-		System.out.printf("ÃÑÁ¡ %d, Æò±Õ %d \n", tot, average);
-		System.out.println("±¹¾î " + kor);
+		
+		// Javaì—ì„œ printf ì‚¬ìš©ë²•ì€ Cì–¸ì–´ì˜ ê·¸ê²ƒê³¼ ë‹¤ë¥´ì§€ ì•Šë‹¤
+		System.out.printf("êµ­ì–´ %d, ì˜ì–´ %d, ìˆ˜í•™ %d\t", kor, eng, math);
+		System.out.printf("ì´ì  %d, í‰ê·  %d \n", tot, average);
+		System.out.println("êµ­ì–´ " + kor);
 
 	}
 
