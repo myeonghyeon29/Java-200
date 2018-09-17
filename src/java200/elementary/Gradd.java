8048c9ee0e8e2b8b8c20dddcf01373dc9139bda3
YanjuLee <43159295+YanjuLee@users.noreply.github.com>
2018-09-17 17:23:40 +0900

    Java에서 printf 사용

--- src/java200/elementary/Gradd.java
+++ src/java200/elementary/Gradd.java
@@ -9,7 +9,7 @@ public class Gradd {
 		int tot = kor + eng + math;
 		int average = tot / 3;
 		
-		// Java에서 printf 사용법은 C언어의 그것과 다르지 않다
+		// Java에서 printf 사용법은 C언어의 그것과 다르지 않다.
 		System.out.printf("국어 %d, 영어 %d, 수학 %d\t", kor, eng, math);
 		System.out.printf("총점 %d, 평균 %d \n", tot, average);
 		System.out.println("국어 " + kor);
