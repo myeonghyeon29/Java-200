a1591fe2f104994d7aa71b6a39904bac42911d27
YanjuLee <43159295+YanjuLee@users.noreply.github.com>
2018-09-18 13:57:00 +0900

    주석 사용

--- src/java200/elementary/JavaComment.java
+++ src/java200/elementary/JavaComment.java
@@ -3,7 +3,7 @@ package java200.elementary;
 public class JavaComment {
 
 	public static void main(String[] args) {
-		// 0~9 »çÀÌÀÇ ÀÓÀÇÀÇ Á¤¼ö¸¦ ¹İÈ¯
+		// 0~9 ì‚¬ì´ì˜ ì„ì˜ì˜ ì •ìˆ˜ë¥¼ ë°˜í™˜, //ìœ¼ë¡œ ì£¼ì„ ì‚¬ìš©
 		int makeOne = (int)(Math.random() * 10);
 		System.out.println(makeOne);
 
