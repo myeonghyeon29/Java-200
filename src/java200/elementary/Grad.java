ef0686bf80ad535514b19dd284b4ac37d97c4ca0
YanjuLee <43159295+YanjuLee@users.noreply.github.com>
2018-09-18 13:50:19 +0900

    배열 사용

--- src/java200/elementary/Grad.java
+++ src/java200/elementary/Grad.java
@@ -3,12 +3,13 @@ package java200.elementary;
 public class Grad {
 
 	public static void main(String[] args) {
+		// C언어와는 다르게 자료형과 변수 사이에 []를 넣는다
 		int[] grade = {90, 80, 89};
 		int tot = grade[0] + grade[1] + grade[2];
 		int average = tot / 3;
-		System.out.println("���� " + grade[0] + " ���� " + grade[1] +
-				" ���� " + grade[2]);
-		System.out.println("���� " + tot + " ��� " + average);
+		System.out.println("±¹¾î " + grade[0] + " ¿µ¾î " + grade[1] +
+				" ¼öÇÐ " + grade[2]);
+		System.out.println("ÃÑÁ¡ " + tot + " Æò±Õ " + average);
 
 	}
 
