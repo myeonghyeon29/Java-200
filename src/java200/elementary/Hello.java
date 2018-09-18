883f86cd999429fada53ccebc76df36261890a11
YanjuLee <43159295+YanjuLee@users.noreply.github.com>
2018-09-18 13:53:48 +0900

    static 메소드

--- src/java200/elementary/Hello.java
+++ src/java200/elementary/Hello.java
@@ -2,13 +2,15 @@ package java200.elementary;
 
 public class Hello {
 	
+	// static을 쓰면 객체 생성 없이 사용 가능
 	public static void showHello() {
 		System.out.println("*****************************");
-		System.out.println("*  �� ���α׷��� Java200�� ����          *");
+		System.out.println("*  ÀÌ ÇÁ·Î±×·¥Àº Java200ÀÌ ¸¸µë          *");
 		System.out.println("*****************************");
 	}
 
 	public static void main(String[] args) {
+		// Hello 객체를 생성하지 않아도 메소드에 접근 가능
 		showHello();
 	}
 
