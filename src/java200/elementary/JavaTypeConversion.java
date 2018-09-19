7783907d26573cd18a80415ed87e18f7d2be814a
YanjuLee <43159295+YanjuLee@users.noreply.github.com>
2018-09-19 13:26:40 +0900

    객체 형변환

--- src/java200/elementary/JavaTypeConversion.java
+++ src/java200/elementary/JavaTypeConversion.java
@@ -6,6 +6,7 @@ public class JavaTypeConversion {
 		String slat = "   37.52127220511242   ";
 		//String slat = "hello";
 		
+		// trim()은 공백을 제거한다, parseXXX()를 통해 변환한다
 		double latitude = Double.parseDouble(slat.trim());
 		System.out.println(latitude);
 
