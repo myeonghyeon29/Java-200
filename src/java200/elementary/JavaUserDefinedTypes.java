e345c96963f6d92274e4e66b38abe318666384f4
YanjuLee <43159295+YanjuLee@users.noreply.github.com>
2018-09-19 13:27:30 +0900

    사용자 정의 클래스 사용

--- src/java200/elementary/JavaUserDefinedTypes.java
+++ src/java200/elementary/JavaUserDefinedTypes.java
@@ -11,6 +11,7 @@ public class JavaUserDefinedTypes {
 		double latitude = 37.5212352341;
 		double longitude = 127.0074472890625;
 		
+		// 사용자 정의 클래스 사용
 		JLocation jloc = new JLocation();
 		jloc.lat = latitude;
 		jloc.lng = longitude;
