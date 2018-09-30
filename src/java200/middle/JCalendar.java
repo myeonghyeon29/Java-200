2f08b08dc23c3445f9e86652bc795ca522a4d032
YanjuLee <43159295+YanjuLee@users.noreply.github.com>
2018-09-30 11:17:17 +0900

    commit

--- src/java200/middle/JCalendar.java
+++ src/java200/middle/JCalendar.java
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-package java200.middle;
 
-public class JCalendar {
-	
-	public boolean isLeapYear(int year) {
-		boolean isS = false;
-		if ( ((year%4 == 0) && (year%100 != 0)) 
-				|| (year%400 == 0) ) {
-			isS = true;
-		}
-		
-		return isS;
-	}
-
-	public static void main(String[] args) {
-		JCalendar hc = new JCalendar();
-		System.out.println(hc.isLeapYear(2018));
-
-	}
-
-}
-=======
 package java200.middle;
 
 public class JCalendar {
@@ -42,4 +20,4 @@ public class JCalendar {
 	}
 
 }
->>>>>>> refs/remotes/origin/master
+
--- src/java200/middle/Temperature.java
+++ src/java200/middle/Temperature.java
@@ -1,31 +1,15 @@
-<<<<<<< HEAD
+
 package java200.middle;
 
 public class Temperature {
 
 	public static void main(String[] args) {
-		// 0µµ ºÎÅÍ 100µµ±îÁö º¯°æ 
+		// 0ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ 100ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ 
 		for (int i=0; i<101; i++) {
 			double fahrenheit = 9.0/5*i + 32;
-			System.out.printf("¼·¾¾ %dµµ = È­¾¾ %.2fµµ \n", i, fahrenheit);
+			System.out.printf("ï¿½ï¿½ï¿½ï¿½ %dï¿½ï¿½ = È­ï¿½ï¿½ %.2fï¿½ï¿½ \n", i, fahrenheit);
 		}
 
 	}
 
 }
-=======
-package java200.middle;
-
-public class Temperature {
-
-	public static void main(String[] args) {
-		// 0µµ ºÎÅÍ 100µµ±îÁö º¯°æ 
-		for (int i=0; i<101; i++) {
-			double fahrenheit = 9.0/5*i + 32;
-			System.out.printf("¼·¾¾ %dµµ = È­¾¾ %.2fµµ \n", i, fahrenheit);
-		}
-
-	}
-
-}
->>>>>>> refs/remotes/origin/master
