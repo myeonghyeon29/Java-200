5fcd2136e588aa69531a99dcb268f7f8b9705717
YanjuLee <43159295+YanjuLee@users.noreply.github.com>
2018-09-30 11:05:07 +0900

    상수 사용, 멤버 메소드 사용

--- src/java200/middle/_30_BioCalendar.java
+++ src/java200/middle/_30_BioCalendar.java
@@ -1,20 +1,23 @@
  package java200.middle;
 
-public class BioCalendar {
+public class _30_BioCalendar {
 
+	// 상수는 final 기호를 붙혀준다
 	public static final int PHYSICAL = 23;
 	
 	/*public static double getBioRhythm(long days, int index, int max) {
 		return max * Math.sin((days % index) * 2 * Math.PI / index);
 	}*/
 	
+	// 멤버 메소드 사용, non-static
 	public double getBioRhythm(long days, int index, int max) {
 		return max * Math.sin((days % index) * 2 * Math.PI / index);
 	}
 	
 	public static void main(String[] args) {
 		int days = 1200;
-		BioCalendar bio = new BioCalendar();
+		// 멤버 메소드를 사용하려면 객체를 통해 호출한다
+		_30_BioCalendar bio = new _30_BioCalendar();
 		double phyval = bio.getBioRhythm(days, PHYSICAL, 100);
 		System.out.println(phyval);
 
